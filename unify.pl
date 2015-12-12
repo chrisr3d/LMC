d9558c42ffa828df290568b91ad7aeceb4dcc7dd
Chris Studer <Chris Studer>
2015-12-12 22:31:17 +0100

    je me suis inspiré de quelques tuto et exemples, sinon j'aurais jamais réussi à écrire tout ça

--- unify.pl
+++ unify.pl
@@ -1,85 +1,95 @@
-:- op(20, xfy, [?=]).
+:- op(20,xfy,?=).
+
+% Prédicats d'affichage fournis
+
+% set_echo: ce prédicat active l'affichage par le prédicat echo
+set_echo :- assert(echo_on).
+
+% clr_echo: ce prédicat inhibe l'affichage par le prédicat echo
+clr_echo :- retractall(echo_on).
+
+% echo(T): si le flag echo_on est positionné, echo(T) affiche le terme T
+%          sinon, echo(T) réussit simplement en ne faisant rien.
+
+echo(T) :- echo_on, !, write(T).
+echo(_).
+
 
 print(Term) :-
-	current_prolog_flag(print_write_options, Options), !, 
+	current_prolog_flag(print_write_options, Options), !,
 	write_term(Term, Options).
 
-unifie(P) :-
-	reduit(_, _, P, Q),
-	print(Q).
 
-decompose(E) :-
-	compound_name_arity(E, ?=, _),
-	arg(1, E, X),
-	arg(2, E, Y),
-	compound_name_arity(X, Z, A),
-	compound_name_arity(Y, Z, A).
+/* occur_check(V,T) permet de vérifier si la variable V est contenue dans le terme T */
 
-decompose(E, P, Q) :-
-	print(E),
-	union(E, K, P),
-	print(K),
-	arg(1, E, I),
-	arg(2, E, J),
-	arg(N, I, L),
-	arg(N, J, D),
-	
-	union([L ?= I], K, Q).
+occur_check(V,T) :- V==T -> !; compound(T), functor(T,N,A), ocheck(V,T,A),!.
 
-occur_check(V,T) :-
-	V==T -> !;
-	compound(T),
-	functor(T,N,A),
-	ocheck(V,T,A),!.
+ocheck(V,T,A) :- A==1 -> arg(1,T,X), occur_check(V,X); arg(A,T,X), occur_check(V,X); A2 is (A-1), ocheck(V,T,A2),!.
 
-ocheck(V,T,A) :-
-	A==1 -> arg(1,T,X),
-	occur_check(V,X);
-	arg(A,T,X),
-	occur_check(V,X);
-	A2 is (A-1),
-	ocheck(V,T,A2),!.
 
-check(V,T) :-
-	occur_check(V,T),!.
 
-% reduit(R,E,P,Q) :
-%	Transforme le système d'équations P en le système d'équations Q par application de la règle de transformation R à l'équation E.
-reduit(R, E, P, Q) :- 
-	.
+/* regle(E,R) : Détermine la règle de transformation R qui peut s'appliquer à l'équation E. */
 
-% regle(E,R) : 
-%	Détermine la règle de transformation R qui s'applique à l'équation E.
+regle((X ?= T),rename) :- var(T),!.
 
-% La règle Rename peut être appliquée à l'équation X = T si T est une variable.
-regle((X ?= T),rename) :-
-	var(T),!.
+regle((X ?= T),simplify) :- atomic(T),!.
 
-% La règle Simplify peut être appliquée à l'équation X = T si T est une constante.
-regle((X ?= T),simplify) :-
-	atomic(T),!.
+regle((X ?= T), check) :- X \== T, occur_check(X,T),!.
 
-% Check est la règle qui intervient au niveau de l'équation X = T si X est différent de T et que X apparaît dans T
-regle((X ?= T), check) :-
-	X \== T,
-	occur_check(X,T),!.
+regle((X ?= T), expand) :- compound(T), not(occur_check(X,T)),!.
 
-% La règle Expand s'applique à l'équation X = T si T est composé et que X n'apparaît pas dans T
-regle((X ?= T), expand) :-
-	compound(T),
-	not(occur_check(X,T)),!.
+regle((X ?= T), decompose) :- compound(X), compound(T), functor(X,A1,N1), functor(T,A2,N2), A1 == A2, N1 == N2,!.
 
-% On peut décomposer une équation S = T lorsque S et T sont composés, et que S et T ont le même nombre d'arguments
-regle((S ?= T), decompose) :-
-	compound(S), compound(T),
-	functor(S,_,A1), functor(T,_,A2),
-	A1 == A2,!.
+regle((T ?= X), orient) :- nonvar(T),!.
 
-% La règle Orient est applicable à l'équation T = X lorsque T n'est pas une variable.
-regle((T ?= X), orient) :-
-	nonvar(T),!.
+regle((X ?= T), clash) :- compound(X), compound(T), functor(X,_,N1), functor(T,_,N2), N1 \== N2,!;
+	compound(X), compound(T), functor(X,A1,_), functor(T,A2,_), A1 \== A2,!.
 
-% La règle Clash est applicable à l'équation S = T lorsque S et T sont composés, et que soit S et T n'ont pas le même nombre d'arguments, soit n'ont pas le même nom.
-regle((S ?= T), clash) :-
-	compound(S), compound(T), functor(S,_,N1), functor(T,_,N2), N1 \== N2,!;
-	compound(S), compound(T), functor(S,A1,_), functor(T,A2,_), A1 \== A2,!.
+
+/* reduit(R,E,P,Q) : Transforme le système d'équations P en le système d'équations Q par application de la règle de transformation R à l'équation E.*/
+
+reduit(R,(X ?= T),P,Q,P1,Q1) :- R==rename, echo(system  : [X ?= T|P]), nl, echo(R), echo( : X ?= T),
+										nl, substitution(X,T,P,P1), Q1=[X=T|Q2] substitution2(X,T,Q,Q2), !.
+
+reduit(R,(X ?= T),P,Q,P1,Q1) :- R==simplify, echo(system  : [X ?= T|P]), nl, echo(R), echo( : X ?= T),
+										nl, substitution(X,T,P,P1), Q1=[X=T|Q2] substitution2(X,T,Q,Q2), !.
+
+reduit(R,(X ?= T),P,Q,P,Q) :- R==check, echo(system : [X ?= T|P]), nl, echo(R), echo( : X ?= T), nl, fail, !.
+
+reduit(R,(X ?= T),P,Q,P1,Q1) :- R==expand, echo(system  : [X ?= T|P]), nl, echo(R), echo( : X ?= T), nl,
+										substitution(X,T,P,P1), Q1=[X=T|Q2] substitution2(X,T,Q,Q2), !.
+
+reduit(R, (X ?= T),P,Q,[T ?= X|P],Q) :- R==orient, echo(system  : [X ?= T|P]), nl, echo(R), echo( : X ?= T), nl, !.
+
+reduit(R,(X ?= T),P,Q,P1,Q) :- R==decompose, echo(system  : [X ?= T|P]), nl, echo(R), echo( : X ?= T), nl,
+										functor(X,_,A), liste_arguments(X ?= T,A,[],L), concat(L,P,P1), !.
+
+reduit(R,(X ?= T),P,Q,P,Q) :- R==clash, echo(system : [X ?= T|P]), nl, echo(R), echo( : X ?= T), nl, fail, !.
+
+
+
+substitution(_,_,[],[]) :- !.
+substitution(X,T,[A ?= B|P],[A2 ?= B2|P2]) :- substitution_terme(X,T,A,A2), substitution_terme(X,T,B,B2),
+												substitution(X,T,P,P2).
+substitution2(_,_,[],[]) :- !.
+substitution2(X,T,[A ?= B|P],[A2 ?= B2|P2]) :- substitution_terme(X,T,A,A2), substitution_terme(X,T,B,B2),
+												substitution2(X,T,P,P2).
+substitution_terme(X,T,A,T) :- not(compound(A)), A==X,!.
+substitution_terme(X,T,A,A) :- not(compound(A)), A\==X,!.
+substitution_terme(X,T,A,A2) :- compound(A), functor(A,_,NA), substitution_argument(X,T,A,NA,A2),!.
+substitution_argument(X,T,A,N,A2) :- N==1 -> functor(A,NM,AR), arg(1,A,VA), substitution_terme(X,T,VA,V), functor(A2,NM,AR), arg(1,A2,V),!;
+									functor(A,NM,AR), arg(N,A,VA), substitution_terme(X,T,VA,V), functor(A2,NM,AR), arg(N,A2,V), N2 is (N-1), substitution_argument(X,T,A,N2,A2).
+
+
+liste_arguments(X ?= T,N,L,L2) :- N==1 -> arg(1,X,A), arg(1,T,B), L2=[A ?= B|L],! ;
+									N2 is (N-1), arg(N,X,A), arg(N,T,B), liste_arguments(A ?= B,N2,[A ?= B|L],L2).
+
+concat([],X,X).
+concat([X|Q],Y,[X|P]) :- concat(Q,Y,P).
+
+
+
+unifieRes([],Q) :- nl, print(Q),!.
+unifieRes([X|P],Q) :- regle(X,R), reduit(R,X,P,Q,P1,Q1), unifieRes(P1,Q1).
+unifie([]) :- !.
+unifie([X|P]) :- regle(X,R), reduit(R,X,P,[],P1,Q1), unifieRes(P1,Q1).
